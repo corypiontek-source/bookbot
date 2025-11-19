9f3a22ed7f0b77e0f17c6722d61fa4a100d471f8
piontekcd <cory.piontek@gmail.com>
2025-11-18 22:35:08 -0500

    :F final

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_path =sys.argv[1]
-
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
